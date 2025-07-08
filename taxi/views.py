e0a829e50e3bc72a9e8c1aa9739d4ad0d70b2a86
msi <lg.mobile0808@gmail.com>
2025-07-08 12:03:49 +0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views import View
+from django.views.generic import View, ListView, DetailView
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -16,29 +16,36 @@ def index(request):
     return render(request, "taxi/index.html", context=context)
 
 
-class ManufacturerListView(View):
+class ManufacturerListView(ListView):
     model = Manufacturer
     queryset = Manufacturer.objects.all().order_by("name")
     paginate_by = 5
+    template_name = 'taxi/manufacturer_list.html'
+    context_object_name = 'manufacturer_list'
 
 
-class CarListView(View):
+class CarListView(ListView):
     model = Car
     paginate_by = 5
-    template_name = "taxi/car_list.html"
-    context_object_name = "car_list"
-    queryset = Car.objects.select_related("manufacturer")
+    template_name = 'taxi/car_list.html'
+    context_object_name = 'car_list'
+    queryset = Car.objects.select_related('manufacturer')
 
 
-class CarDetailView(View):
+class CarDetailView(DetailView):
     model = Car
+    template_name = 'taxi/car_detail.html'
+    context_object_name = 'car'
 
-
-class DriverListView(View):
+class DriverListView(ListView):
     model = Driver
     paginate_by = 5
+    template_name = 'taxi/driver_list.html'
+    context_object_name = 'driver_list'
 
 
-class DriverDetailView(View):
+class DriverDetailView(DetailView):
     model = Driver
+    template_name = 'taxi/driver_detail.html'
+    context_object_name = 'driver'
     queryset = Driver.objects.select_related("manufacturer")
